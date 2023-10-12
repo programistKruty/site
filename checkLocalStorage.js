c2f7d6d601d25dfce9520e9978b11620b6d10a30
xuy123 <xyu123@gmail.com>
2023-10-12 11:41:11 +0200

    AXXAXAXAXAXA

--- checkLocalStorage.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // Проверяем, залогинен ли пользователь
-    const isUserLoggedIn = localStorage.getItem('isUserLoggedIn');
-
-    if (isUserLoggedIn === 'true') {
-        // Если пользователь уже залогинен, перенаправляем его на главную страницу
-        window.location.href = '../account/account.html';
-    }
-});
